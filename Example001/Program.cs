a66d2094f4f6b5a9bea8f6b0580ddb78028cd8d2
andreybel1989 <bav7269213@gmail.com>
2023-12-28 00:01:13 +0300

    create function GetArray

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+string[] GetArray()
+{
+    Console.WriteLine("Enter the numbers of elements");
+    int arrayLength = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[arrayLength];
+    int index = 1;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.WriteLine($"Input {index} element");
+        array[i] = Console.ReadLine();
+        index++;
+    }
+
+    return array;
+}
